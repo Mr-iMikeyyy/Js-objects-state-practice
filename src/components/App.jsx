bf16f6639a79174f14bfb4a48a7111ca1fe46066
Mr-iMikeyyy <56235937+Mr-iMikeyyy@users.noreply.github.com>
2021-06-18 10:26:30 -0400

    getting somewhere but there's a syntax error somewhere and im perplexed

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,6 +7,33 @@ function App() {
     email: ""
   });
 
+  function handleChange(event) {
+    const newValue = event.target.value;
+    const inputName = event.target.name;
+    setContact((prevValue) => {
+      if (inputName === "fName") {
+        return 
+        {
+          fName: newValue,
+          lName: prevValue.lName,
+          email: prevValue.email
+        };
+      } else if (inputName === "lName") {
+        return {
+          fName: prevValue.fName,
+          lName: newValue,
+          email: prevValue.email
+        };
+      } else if (inputName === "email") {
+        return {
+          fName: prevValue.fName,
+          lName: prevValue.lName,
+          email: newValue
+        };
+      }
+    });
+  }
+
   return (
     <div className="container">
       <h1>
@@ -14,9 +41,9 @@ function App() {
       </h1>
       <p>{contact.email}</p>
       <form>
-        <input name="fName" placeholder="First Name" />
-        <input name="lName" placeholder="Last Name" />
-        <input name="email" placeholder="Email" />
+        <input onChange={handleChange} name="fName" placeholder="First Name" />
+        <input onChange={handleChange} name="lName" placeholder="Last Name" />
+        <input onChange={handleChange} name="email" placeholder="Email" />
         <button>Submit</button>
       </form>
     </div>
